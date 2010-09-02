8589ae57a0834b9ed4dbc06b5562afd33620daf3
Mark Hayes <mark@deployfx.com>
2010-09-02 13:23:32 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timelog::Application.config.secret_token = '658e087b11121bbe18342f89758c05e609efb2ea1d73a2ad05c4ec1338db39da99def267082a24fda5f9b699227f9510223bb985e93c48ef05fe10c145492894'
