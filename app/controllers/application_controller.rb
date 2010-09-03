9670c0f34f1839f2d7a910eecf164186d895f1f0
Mark Hayes <mark@deployfx.com>
2010-09-02 19:38:30 -0700

    added basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+  
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "csc191" && password == "csus2010"
+    end
+  end
 end
