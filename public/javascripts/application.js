0f6b3cbed56410d26ef1a67d3a2934fcc667ed81
Mark Hayes <mark@deployfx.com>
2010-09-03 12:41:40 -0700

    turned all links and submit buttons into jquery-ui buttons

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,4 +2,5 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(function() {
 	$(':input.datefield').datepicker({dateFormat:'yy-mm-dd', defaultDate:Date.today});
+	$(':submit, a').button();
 });
